783db7a05a4d1d53a276327aac8dd7d0efe08843
Camila Soto <lilianxstoo@gmail.com>
2025-09-16 15:52:35 -0600

    16/09/2025

--- configs/app.js
+++ configs/app.js
@@ -4,8 +4,10 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import {dbConnection } from './db.js';
 
 import 'dotenv/config';
+
 
 const middlewares = (app) => {
     app.use(express.json());
@@ -15,15 +17,24 @@ const middlewares = (app) => {
     app.use(morgan('dev'));
 }
 
-export const initServer = () => {
+const conectarDB = async () =>{
+    try{
+        await dbConnection();
+    }catch(error){
+        console.log(`Error al conectador la db: ${error.message}`)
+    }
+}
+
+export const initServer = async() => {
     const app = express();
 
-
     try{
+        middlewares(app)
+        await conectarDB()
         app.listen(process.env.PORT, () =>{
             console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
         })
-         }catch(errro){
+         }catch(error){
             console.log(`Error al servidor: ${error}`);
     }
-}
+} 
