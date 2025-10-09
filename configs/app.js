c325d3882d885c92e02fea0e24e1afbcf7d49549
Camila Soto <lilianxstoo@gmail.com>
2025-10-09 16:03:23 -0600

    09/10/2025

--- configs/app.js
+++ configs/app.js
@@ -6,16 +6,29 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import {dbConnection } from './db.js';
 import userModel from '../src/users/user.model.js';
-
+import authRoutes from '../src/auth/auth.routes.js';
+import requestLimit from '../middlewares/request-limit.js';
 import 'dotenv/config';
 
 
 const middlewares = (app) => {
     app.use(express.json());
     app.use(express.urlencoded({extended: false}))
-    app.use(cors());
-    app.use(helmet());
+    app.use(cors({
+        origin: '*',
+        credentials: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content - Type', 'Authorization']
+    }));
+    app.use(helmet({
+        crossOrigiResourcePolicy: { policy: "cross-orgin"},
+        crossOriginEmbedderPolicy: false
+    }));
     app.use(morgan('dev'));
+    app.use(requestLimit)
+}
+
+const routes = (app) => {
+    app.use('/api/auth', authRoutes)
 }
 
 const conectarDB = async () =>{
@@ -31,6 +44,7 @@ export const initServer = async() => {
 
     try{
         middlewares(app)
+        routes(app)
         await conectarDB()
         app.listen(process.env.PORT, () =>{
             console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,25 +1,67 @@
-import User from '../users/users.model.js'
-import { hash } from 'argon2'
+import User from '../users/user.model.js'
+import { hash, verify } from 'argon2'
+import { generarJWT } from '../../helpers/JWT-generate.js'
 
-export const register = async (req, res) => {
+export const register = async (req,res) => {
     try{
-         const data = req.body
+        const data = req.body
 
-         let profilePicture = req.fileRelativePath || 'profiles/default-avatar.png'
-         const encryptedPassword = await hash(data.password)
+        let profilePicture = req.fileRelativePath || 'profiles/defaul-awatar.png'
+        const encryptedPassword = await hash (data.password)
 
-         const newUser = await User.create({
+        const newUser = await User.create({
             name: data.name,
             surname: data.surname,
             username: data.username,
             email: data.email,
             password: encryptedPassword,
             profilePicture
-         })
+        })
+        return res.status(200).json({
+            message: "Usuario registrado corectamente",
+            userDetails: {
+                user: newUser.username,
+                email: newUser.email,
+
+            },
+        });
     }catch(error){
         return res.status(500).json({
-            message: 'Error al registrar el usuario',
+            message:'Error al registrar el usuario',
             err: error.message
         })
     }
 }
+export const login = async (req,res) => {
+    const { email,password,username} = req.body;
+    try{
+        const lowerEmail = email? email.toLowerCase(): null;
+        const lowerUsername = username? username.toLowerCase(): null;
+
+        const user = await User.findOne({
+            $or:[{email:lowerEmail},{username: lowerUsername}],
+        });
+        if (!user){
+            return res.status(401).json({message:"Credenciales incorrectas"});            
+        }
+        const validPassword = await verify(user.password, password);
+        if (!validPassword){
+            return res.status(401).json({message: "Credenciales incorrectas"});
+        }
+        const token = await generarJWT(user.id, user.email);
+        return res.status(200).json({
+            mesage: "Inicio de sesión exitoso",
+            userDetails: {
+                username: user.username,
+                token:token,
+                profilePicture: user.profilePicture,
+                uid: user.id
+            },
+        });
+    }   catch(error){
+        return res.status(500).json({
+            message: "Error del servidor",
+            error: error.message,
+        });
+    }  
+};
